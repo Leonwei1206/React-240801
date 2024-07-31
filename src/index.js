dd60d0127c258c3793dc0456fc7018147e79d5fa
Leonwei1206 <68678844+Leonwei1206@users.noreply.github.com>
2024-07-31 19:11:31 +0800

    first

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 //import './index.css';
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Introduce from "./Introduce";
 import Topics from "./Topics";
 import './index.css';
@@ -12,13 +12,13 @@ import './index.css';
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   
-    <BrowserRouter>
+    <HashRouter>
      <Routes>
         <Route path="/" element={<App />} />
         <Route path="/introduce" element={<Introduce />} />
         <Route path="/Topics" element={<Topics />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
      
 );
 
